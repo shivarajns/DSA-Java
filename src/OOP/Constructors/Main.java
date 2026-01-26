3b192f91fd5bdc1d1159982e8aa9ce17c2aca4b0
Shivaraju NS <shivarajsmurthy@gmail.com>
2026-01-26 10:00:00 +0530

    DSA Day- 02, OOP's -> Constructor in Object-Oriented Programming.

---
+++ src/OOP/Constructors/Main.java
@@ -0,0 +1,9 @@
+package OOP.Constructors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        new constructors("Shivaraj", 2);
+
+    }
+}
---
+++ src/OOP/Constructors/constructors.java
@@ -0,0 +1,12 @@
+package OOP.Constructors;
+
+public class constructors {
+
+    constructors(String name, int id){
+
+        System.out.println(name + " id is " + id);
+
+    }
+
+
+}
