791d290124ab9e78f31a74271bb7a3b4de031d6e
Shivaraju NS <shivarajsmurthy@gmail.com>
2026-03-04 23:04:12 +0530

    Practice Problem: Find Weather the given number is Even or Odd.

---
+++ src/PracticeProblems/Array/EvenAndOdd/EvenandOdd.java
@@ -0,0 +1,14 @@
+package PracticeProblems.Array.EvenAndOdd;
+
+import java.sql.SQLOutput;
+
+public class EvenandOdd {
+
+    EvenandOdd(int num){
+        if(num % 2 == 0){
+            System.out.println(num + " is an Even Number");
+        }
+        else {
+            System.out.println(num + " is an Odd Number");        }
+    }
+}
---
+++ src/PracticeProblems/Array/EvenAndOdd/Main.java
@@ -0,0 +1,9 @@
+package PracticeProblems.Array.EvenAndOdd;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EvenandOdd a = new EvenandOdd(10);
+
+    }
+}
